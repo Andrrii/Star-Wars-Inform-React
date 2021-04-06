b16798981a0169f7d82457235c4f43997669152f
Andrii <butsaandrii@gmail.com>
2021-04-06 17:47:10 +0300

    add logic to component 'random-planet' and add some new features

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./spinner.css"
+
+const Spinner = () => {
+    return (
+        <div class="cssload-loader">
+            <div class="cssload-inner cssload-one"></div>
+            <div class="cssload-inner cssload-two"></div>
+            <div class="cssload-inner cssload-three"></div>
+        </div>
+    )
+}
+
+export default Spinner
